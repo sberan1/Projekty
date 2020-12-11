a56de8d90ec5a8a3e9538879a2cb471fcdefde69
stepan <stepan.beran@outlook.com>
2020-12-11 20:46:36 +0100

    .

---
+++ Xam1/Xam1/Models/AMan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xam1.Models
+{
+    public class AMan
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
